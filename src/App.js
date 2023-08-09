6a782b426d3c17a5a33ab9828615bfa080ae3b48
Lucas D. Gaio <gaio.lucas0@gmail.com>
2023-08-08 22:39:40 -0300

    adsdfsdf

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleSubmit() {
+    console.log('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" name="username" />
+        <br />
+        <input type="password" name="password" />
+        <br />
+        <button type='submit' onClick={handleSubmit}>Entrar</button>
+      </form>
     </div>
   );
 }
