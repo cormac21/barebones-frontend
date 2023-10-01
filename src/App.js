503f8b051326fd8a175e701bf3b446ba1b374de9
Lucas D. Gaio <gaio.lucas0@gmail.com>
2023-10-01 19:12:28 -0300

    checkpoint before my pc goes kaput again

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
 
-  function handleSubmit() {
-    console.log('')
+  const [file, setFile] = useState(null)
+  const [msg, setMsg] = useState(null)
+  const [images, setImages] = useState([])
+
+  function handleUpload() {
+    if (!file) {
+      setMsg("No file selected!")
+      return
+    }
+    const fd = new FormData()
+    console.log('fd', fd)
+    fd.append('file', file)
   }
 
   return (
-    <div className="App">
-      <form>
-        <input type="text" name="username" />
-        <br />
-        <input type="password" name="password" />
-        <br />
-        <button type='submit' onClick={handleSubmit}>Entrar</button>
-      </form>
+    <div className="app-container">
+      <div className='toolbar'>
+        <h1>Lucas' photo uploads</h1>
+      </div>
+      <div className='content'>
+        <div className='row'>
+          <h2>Upload your photo here!</h2>
+
+        </div>
+        <div className='row'>
+          <div className='file-upload'>
+            <div className='file-upload-button'>
+              File: 
+            </div>
+          </div>
+          <label for='file-upload-input' className='file-upload-div'>
+            Envie aqui seu arquivo
+          </label>
+          <input 
+              name='file-upload-input'
+              className='file-upload-field'
+              onChange={(e) => setFile(e.target.files[0])}
+              type='file'
+            />
+          <button onClick={handleUpload}>Save!</button>
+        </div>
+        {msg &&
+          <div className='row'>
+            {msg}
+          </div>
+        }
+        {images &&
+          <div>
+
+          </div>
+        }
+      </div>
     </div>
   );
 }
